b06cb89b7a29eeeb10de36af5e4f59391654414c
Ricardo Chi <ricardojch.12@gmail.com>
2025-11-23 17:47:26 -0500

    Nuevos Botones UI

--- Assets/Scripts/UI/TutorialManager.cs
+++ Assets/Scripts/UI/TutorialManager.cs
@@ -55,7 +55,7 @@ public class TutorialManager : MonoBehaviour
         }
         else
         {
-            nextButtonText.text = "Siguiente >>";
+            nextButtonText.text = "Siguiente";
         }
     }
 
